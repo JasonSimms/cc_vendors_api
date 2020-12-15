21779b1419bb490ea88e90b2892ef55984381261
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 18:51:06 +0100

    Deploy ts

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3001"
-};
+// var corsOptions = {
+//   origin: "http://localhost:3001"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -18,18 +18,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // DB connect
 const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
 
 // simple route
 app.get("/", (req, res) => {
