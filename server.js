40da04a0a0aa69fea99abf860e29cd5078c0b5bc
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 19:29:27 +0100

    troubleshooting

--- server.js
+++ server.js
@@ -45,5 +45,5 @@ app.get("/", (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT} and ${process.env.MONGOATLAS_URL}.`);
 });
