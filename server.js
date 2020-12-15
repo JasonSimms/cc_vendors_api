3d1367eeb340f4524e3ad95a2561a1dfdbe25e2c
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 19:31:34 +0100

    troubleshooting

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // DB connect
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+// const db = require("./app/models");
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
 
 // simple route
 app.get("/", (req, res) => {
@@ -45,5 +45,6 @@ app.get("/", (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} and ${process.env.MONGOATLAS_URL}.`);
+  console.log(`Server is running on port ${PORT} and ${process.env.MONGOATLASS_URL}.`);
+  console.log(process.env)
 });
