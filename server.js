607fc872bd8c6670743304c80bfefe46986a3ee6
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 19:22:27 +0100

    Process info?

--- server.js
+++ server.js
@@ -18,22 +18,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // DB connect
 const db = require("./app/models");
-// db.mongoose
-//   .connect(db.url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => {
-//     console.log("Connected to the database!");
-//   })
-//   .catch(err => {
-//     console.log("Cannot connect to the database!", err);
-//     process.exit();
-//   });
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome" });
+  res.json({ message: "Welcome", env: process.env.MONGOATLAS_URL, process: process.env });
   // console.log("hello");
 });
 
