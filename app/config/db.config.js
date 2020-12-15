26f5d349b203112539bf03f83112b9ce3a356b18
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 04:50:17 +0100

    mongo atlas added

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/cc_api"
+    // url: "mongodb://localhost:27017/cc_api"
+    url : `mongodb+srv://admin:<pw>@marketaware.lo5k9.mongodb.net/cc_api?retryWrites=true&w=majority`
   };
