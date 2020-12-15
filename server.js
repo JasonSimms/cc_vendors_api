edabe0b24841054c505c3c16e13e3900b1cbe02e
jasonsimms <simmsthecoder@gmail.com>
2020-12-15 22:29:04 +0100

    TS CORS

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
+// var corsOptions = {
+//   origin: "http://localhost:3000"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
+
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
